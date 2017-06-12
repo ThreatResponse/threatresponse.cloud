37969f4b7c87a5151c12a5d0d1f121c7419db782
andrewkrug <andrewkrug@gmail.com>
2017-06-12 16:51:18 -0700

    remove bad code

--- config.rb
+++ config.rb
@@ -7,46 +7,6 @@ configure :development do
   end
 end
 
-activate :rouge_syntax, :css_class => '.highlight', :line_numbers => false
-
-set :markdown_engine, :redcarpet
-set :markdown, :fenced_code_blocks => true, :smartypants => true
-
-
-#page "/derbycon", :layout => false
-
-#redirect "/derbycon", to: "http://threatresponse-derbycon.s3-website-us-west-2.amazonaws.com/"
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page command
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-with_layout :layout do
-   page "/derbycon/*"
-end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
 ###
 # Helpers
 ###
@@ -65,15 +25,6 @@ helpers do
 
 end
 
-# Change the CSS directory
-# set :css_dir, "alternative_css_directory"
-
-# Change the JS directory
-# set :js_dir, "alternative_js_directory"
-
-# Change the images directory
-# set :images_dir, "alternative_image_directory"
-
 # Build-specific configuration
 configure :build do
   activate :blog do |blog|
@@ -81,7 +32,7 @@ configure :build do
     blog.permalink = "{year}/{title}.html"
   end
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
    activate :minify_javascript
