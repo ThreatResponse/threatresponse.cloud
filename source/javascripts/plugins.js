54d5be9c817922e089271cae3d6c40b255982981
Jeff Parr <jeffparr100@gmail.com>
2017-11-30 08:22:08 -0800

    add google analytics

--- source/javascripts/plugins.js
+++ source/javascripts/plugins.js
@@ -11,4 +11,10 @@ if (!(window.console && console.log)) {
     }());
 }
 
-// Place any jQuery/helper plugins in here.
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-80834855-1', 'auto');
+ga('send', 'pageview');
